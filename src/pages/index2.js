03b7312b6cbc6f42acd059f7ed1b115d7a0ea134
Jashoshul Wathon <jashoshul.wathon@java-valley.com>
2021-04-02 01:00:47 +0700

    Update index2.js

--- src/pages/index2.js
+++ src/pages/index2.js
@@ -17,7 +17,7 @@ const Index = () => {
           <p>I am Jashoshul Wathon, an artist/designer based in Semarang focused on UX/UI design and illustration</p>
           <Link className={style.btn} to="/blog">View work</Link>
         </div>
-        <img className={style.img} src={cover}></img>
+        {/* <img className={style.img} src={cover}></img> */}
       </div>
     </Layout>
   )
